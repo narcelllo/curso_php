db8795261f4c008a470712d21360a049df607939
Marcello Magno <marcello_magno@hotmail.com>
2021-11-08 18:08:51 -0300

    Test numerical

--- unidade07/numeros2.php
+++ unidade07/numeros2.php
@@ -1,6 +1,7 @@
 <?php
     $salario = 1095;
     $gasolina = 4.55;
+    $telefone = "(15)99232-4332"
 ?>
 
 <!doctype html>
@@ -15,12 +16,19 @@
         
         <?php
             // testar se é numérica
-
+            echo " O $salario é um número?" . is_numeric($salario) . "</br>";
+            echo " O $gasolina é um número?" . is_numeric($gasolina). "</br>";
+            echo " O $telefone é um número?" . is_numeric($telefone). "</br>";
 
             // testar se é inteiro
-
+            echo " O $salario é um inteiro?" . is_int($salario) . "</br>";
+            echo " O $gasolina é um inteiro?" . is_int($gasolina). "</br>";
+            echo " O $telefone é um inteiro?" . is_int($telefone). "</br>";
 
             // testar se é float
+            echo " O $salario é um racional?" . is_float($salario) . "</br>";
+            echo " O $gasolina é um racional?" . is_float($gasolina). "</br>";
+            echo " O $telefone é um racional?" . is_float($telefone). "</br>";
         ?>
         
         
