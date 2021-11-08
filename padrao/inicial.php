1a6a9d94c930253626994c66f0c98e9a55341be8
Marcello Magno <marcello_magno@hotmail.com>
2021-11-08 16:22:39 -0300

    Insert php code in page

--- padrao/inicial.php
+++ padrao/inicial.php
@@ -1,10 +1,20 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Curso PHP FUNDAMENTAL</title>
-    </head>
 
-    <body>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP FUNDAMENTAL</title>
+</head>
+
+<body>
+    <?php
+    echo "hello world";
+    ?>
+    <p>
+        <?php
+        echo "hello world";
+        ?>
+    </p>
+</body>
+
 </html>
