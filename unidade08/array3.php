b2a9fdc17bdb62acd2754bccd48e52d0dfb21ff3
Marcello Magno <marcello_magno@hotmail.com>
2021-11-09 17:59:11 -0300

    Array functions 1

--- unidade08/array3.php
+++ unidade08/array3.php
@@ -1,5 +1,5 @@
 <?php
-    $_salada = array["Laranja","Uva","Abacate"];
+    $_salada = array("Laanja", "Uva", "Abacate");
 ?>
 
 <!doctype html>
@@ -11,8 +11,9 @@
 
     <body>
         <?php 
-            $_1telefone = "Matheus Fontenelle"; 
-            echo $_1telefone;
+            echo "existe no array " . in_array("Abacate", $_salada); // retorna se existe no array
+            echo "existe no array " . in_array("abacate", $_salada);
+            echo "existe no array " . array_search("Abacate", $_salada); // retora a posição esta dentro do array
         ?>
     </body>
 </html>
