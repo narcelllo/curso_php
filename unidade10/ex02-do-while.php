70db196dd2755570cd9439810e408bd314754eff
Marcello Magno <marcello_magno@hotmail.com>
2021-11-10 17:33:06 -0300

    do While

--- unidade10/ex02-do-while.php
+++ unidade10/ex02-do-while.php
@@ -5,8 +5,16 @@
     <title>CURSO PHP FUNDAMENTAL</title>
 </head>
 <body>
-    <?php                    
+    <?php
+        $contador = 0;
+        
+        do {
+            $sortear = rand(1,100);
+            echo $sortear . "</br>";
+            $contador ++;
 
+        } while($contador <= 10)
+        
     ?>
 </body>
 </html>
