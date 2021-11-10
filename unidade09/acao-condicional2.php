95f692abe3a8f83e1020866affc3e6430e1e164e
Marcello Magno <marcello_magno@hotmail.com>
2021-11-10 16:31:47 -0300

    Switch PHP

--- unidade09/acao-condicional2.php
+++ unidade09/acao-condicional2.php
@@ -7,6 +7,28 @@
     </head>
     <body>
         <?php 
+            $dia = "quarta";
+
+            switch($dia){
+                case "segunda":
+                    echo "hoje é segunda";
+                    break;
+                case "terça":
+                    echo "hoje é terça";
+                    break;
+                case "quarta":
+                    echo "hoje é quarta";
+                    break;
+                case "quinta":
+                    echo "hoje é quinta";
+                    break;
+                case "sexta":
+                    echo "hoje é sexta";
+                    break;
+                default:
+                    echo "fim de semana!";
+                    //break; opcional na ultima validação
+            }
 
         ?>
     </body>
