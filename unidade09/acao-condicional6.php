7aaedbca946e4e84cba3138dc148abbd0825570d
Marcello Magno <marcello_magno@hotmail.com>
2021-11-10 17:15:57 -0300

    || and && PHP

--- unidade09/acao-condicional6.php
+++ unidade09/acao-condicional6.php
@@ -6,7 +6,24 @@
         <title>CURSO PHP FUNDAMENTAL</title>
     </head>
     <body>
-        <?php 
+        <?php
+            $dia = "domingo";
+            $idade = 18;
+            $sexo = "feminino";
+
+            if($dia == "sabado" || $dia == "domingo"){
+                echo "fim de semana!";
+            } else {
+                echo "dia de semana";
+            }
+
+            if($odade >= 18 && $sexo == "feminino"){
+                echo "pode entrar na festa";
+            } else {
+                echo "festa apenas para mulheres!";
+            }
+
+            
 
         ?>
     </body>
