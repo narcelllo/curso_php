84008aa9b1bba95e988e6991d7675310d6f96d5a
Marcello Magno <marcello_magno@hotmail.com>
2021-11-11 11:56:12 -0300

    Date 02

--- unidade12/ex02-mostrar-hora.php
+++ unidade12/ex02-mostrar-hora.php
@@ -7,9 +7,18 @@
 
     <body>
         <?php
-            date_default_timezone_set('US/Eastern');
+            date_default_timezone_set('Brazil/east');
             $_agora = getdate();
-            print_r($_agora);
+            
+            $segundo = $_agora["seconds"];
+            $minuto = $_agora["minutes"];
+            $hora = $_agora["hours"];
+
+            $dia = $_agora["mday"];
+            $mes = $_agora["mon"];
+            $ano = $_agora["year"];
+
+            echo $hora . ":" . $minuto . ":" . $segundo . " - " . $dia ."/". $mes ."/". $ano; 
         ?>
     </body>
 </html>
