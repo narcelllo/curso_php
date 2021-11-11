23e9d7a585d2fec9711c474cdc024e0378ee5d97
Marcello Magno <marcello_magno@hotmail.com>
2021-11-11 10:23:35 -0300

    Function 03

--- unidade11/ex03-funcao.php
+++ unidade11/ex03-funcao.php
@@ -5,8 +5,20 @@
     <title>CURSO PHP FUNDAMENTAL</title>
 </head>
 <body>
-    <?php 
+    <?php
+        function converterfc($temperatura){
+            return ($temperatura * 1.8) + 32;
+        }
 
+        echo converterfc(32);
+    ?>
+
+    <?php
+        function converterCf($temperatura){
+            return ($temperatura - 32) / 1.8;
+        }
+
+        echo converterCf(32);
     ?>
 </body>
 </html>
