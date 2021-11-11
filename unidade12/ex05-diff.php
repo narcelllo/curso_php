e974057c2fc105312c45c8c287cfb87f416e54ca
Marcello Magno <marcello_magno@hotmail.com>
2021-11-11 18:11:16 -0300

    Date Format

--- unidade12/ex05-diff.php
+++ unidade12/ex05-diff.php
@@ -6,5 +6,23 @@
     </head>
 
     <body>
+        <?php
+        $data1 = new DateTime('2021-01-01');
+        $data2 = new DateTime('2021-11-11');
+        $intervalo = $data1-> diff($data2);
+        
+        ?>
+        <pre>
+            <?php
+            print_r($intervalo);
+            ?>
+        </pre>
+        
+        <pre>
+            <?php
+            print_r($intervalo->format('%a'));
+            ?>
+        </pre>
+        
     </body>
 </html>
