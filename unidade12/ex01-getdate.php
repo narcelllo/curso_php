183096f9525568ef87668e80178eb6298d6356e2
Marcello Magno <marcello_magno@hotmail.com>
2021-11-11 10:32:18 -0300

    Date function

--- unidade12/ex01-getdate.php
+++ unidade12/ex01-getdate.php
@@ -5,6 +5,11 @@
         <title>Curso PHP FUNDAMENTAL</title>
     </head>
 
-    <body>
+    <body
+        <?php 
+            date_default_timezone_set('Brazil/east');
+            $agora = getdate();
+            print_r($agora);
+        ?>
     </body>
 </html>
